ec00b6e00acaa1c5720a42f933c3ce2f15a89efa
sivin-git <iamsivin@gmail.com>
2021-01-15 10:28:44 +0530

    Added React LazyLoadImage in Works page

--- src/component/Works.js
+++ src/component/Works.js
@@ -1,4 +1,6 @@
 import "./Works.css";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import Calculator from "../static/calculator.png";
 import Fishbowl from "../static/fish bowl.png";
 import Chatnow from "../static/chatnow.png";
@@ -21,8 +23,9 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/Calculator">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img
-                        className="workimg" src={Calculator} alt=""/>
+                    <LazyLoadImage
+                        className="workimg" src={Calculator} alt=""
+                        effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Calculator</h1>
@@ -38,7 +41,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={Clock} alt="" />
+                      <LazyLoadImage className="workimg" src={Clock} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Digital Clock</h1>
@@ -55,8 +58,8 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/ChatNow">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img
-                        className="workimg" src={Chatnow} alt=""/>
+                      <LazyLoadImage
+                        className="workimg" src={Chatnow} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">ChatNow</h1>
@@ -72,7 +75,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={FB} alt="" />
+                      <LazyLoadImage className="workimg" src={FB} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Facebook Login</h1>
@@ -89,7 +92,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/Fish-Bowl">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={Fishbowl} alt=""/>
+                      <LazyLoadImage className="workimg" src={Fishbowl} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Fish Bowl</h1>
@@ -105,8 +108,8 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img
-                        className="workimg" src={TicTacToe} alt=""/>
+                      <LazyLoadImage
+                        className="workimg" src={TicTacToe} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Tic-tac-toe</h1>
@@ -122,8 +125,8 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/Color-Switcher">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img
-                        className="workimg" src={Colorswitcher} alt=""/>
+                      <LazyLoadImage
+                        className="workimg" src={Colorswitcher} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Color Switcher</h1>
@@ -139,7 +142,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={Todo} alt=""/>
+                      <LazyLoadImage className="workimg" src={Todo} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Todo App</h1>
@@ -155,7 +158,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/Tip-Calculator">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={Tip} alt="" />
+                      <LazyLoadImage className="workimg" src={Tip} alt="" effect="blur"/>
                       <div className="piece_content">
                         <h1 className="piece_title">Tip Calculator</h1>
                         <p className="piece_description">
@@ -171,7 +174,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/QRCode-Generator">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={QRcode} alt="" />
+                      <LazyLoadImage className="workimg" src={QRcode} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">QR Code Generator</h1>
@@ -187,8 +190,8 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git/Emoji-Switcher">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img
-                        className="workimg" src={Emoji} alt=""/>
+                      <LazyLoadImage
+                        className="workimg" src={Emoji} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Emoji Switcher</h1>
@@ -204,7 +207,7 @@ import Sound from "../static/sound.png";
                 <a href="https://github.com/sivin-git">
                   <div className="piece inner_container">
                     <div className="image">
-                      <img className="workimg" src={Sound} alt="" />
+                      <LazyLoadImage className="workimg" src={Sound} alt="" effect="blur"/>
                     </div>
                     <div className="piece_content">
                       <h1 className="piece_title">Sounds</h1>
