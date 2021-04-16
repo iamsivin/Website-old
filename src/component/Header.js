72f209a224345dc1e85810d783f1c316bc3caf5f
sivin-git <iamsivin@gmail.com>
2021-04-16 18:00:55 +0530

    github link update

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,27 +1,31 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
 import Logo from "../logo.jpeg";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
- const Header = () => {
-    return (
-      <header>
-        <div className="container header-wrap">
-          <div className="block left pro-wrap">
-            <LazyLoadImage className="pro-pic" src={Logo} effect="blur" alt=""/>
-          </div>
-          <div className="block right">
-            <nav className="nav-link">
-              <NavLink activeClassName="active" exact to="/">About </NavLink>
-              <NavLink activeClassName="active" exact to="/Work">Works</NavLink>
-              <a href="https://github.com/sivin-git" className="nav_contact">
-                GitHub
-              </a>
-            </nav>
-          </div>
+const Header = () => {
+  return (
+    <header>
+      <div className="container header-wrap">
+        <div className="block left pro-wrap">
+          <LazyLoadImage className="pro-pic" src={Logo} effect="blur" alt="" />
         </div>
-      </header>
-    );
-  }
+        <div className="block right">
+          <nav className="nav-link">
+            <NavLink activeClassName="active" exact to="/">
+              About{" "}
+            </NavLink>
+            <NavLink activeClassName="active" exact to="/Work">
+              Works
+            </NavLink>
+            <a href="https://github.com/iamsivin" className="nav_contact">
+              GitHub
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
 export default Header;
